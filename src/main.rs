8e1cf79a0547a82b60f889de7bdba8e8a72a9bed
Adam Richardson <ajrich01@gmail.com>
2019-06-02 20:34:47 -0400

    Put entity creation in a loop Still searching for a way to iterate entities from inside a run join

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,37 @@
+extern crate rand;
 extern crate sdl2;
 
+use rand::Rng;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 use sdl2::render::WindowCanvas;
+use specs::world::Entity;
+use specs::{
+    Builder, Component, Entities, Join, ReadStorage, RunNow, System, VecStorage, World,
+    WriteStorage,
+};
 use std::time::Duration;
 
-use specs::{Builder, Component, ReadStorage, RunNow, System, VecStorage, World, WriteStorage};
+#[derive(Debug)]
+struct Cube {
+    x: i32,
+    y: i32,
+    size: u32,
+}
+
+impl Component for Cube {
+    type Storage = VecStorage<Self>;
+}
 
 #[derive(Debug)]
-struct Position {
+struct Velocity {
     x: i32,
     y: i32,
 }
 
-impl Component for Position {
+impl Component for Velocity {
     type Storage = VecStorage<Self>;
 }
 
@@ -24,17 +40,16 @@ struct Render {
 }
 
 impl<'a> System<'a> for Render {
-    type SystemData = ReadStorage<'a, Position>;
+    type SystemData = ReadStorage<'a, Cube>;
 
-    fn run(&mut self, position: Self::SystemData) {
-        use specs::Join;
+    fn run(&mut self, cube: Self::SystemData) {
         self.canvas.set_draw_color(Color::RGB(0, 0, 0));
         self.canvas.clear();
 
         self.canvas.set_draw_color(Color::RGB(255, 255, 255));
-        for position in position.join() {
+        for cube in cube.join() {
             self.canvas
-                .fill_rect(Rect::new(position.x, position.y, 50, 50))
+                .fill_rect(Rect::new(cube.x, cube.y, cube.size, cube.size))
                 .unwrap();
         }
         self.canvas.present();
@@ -44,23 +59,34 @@ impl<'a> System<'a> for Render {
 struct Physics {
     width: u32,
     height: u32,
+    entities: Vec<Entity>,
 }
 
 impl<'a> System<'a> for Physics {
-    type SystemData = WriteStorage<'a, Position>;
+    type SystemData = (
+        Entities<'a>,
+        WriteStorage<'a, Cube>,
+        WriteStorage<'a, Velocity>,
+    );
 
-    fn run(&mut self, mut position: Self::SystemData) {
-        use specs::Join;
-        for position in (&mut position).join() {
-            position.x += 1;
-            position.y += 1;
+    fn run(&mut self, (entities, mut cube, mut velocity): Self::SystemData) {
+        for (ent, cube, vel) in (&*entities, &mut cube, &mut velocity).join() {
+            cube.x += vel.x;
+            cube.y += vel.y;
 
-            if (position.x + 50) > self.width as i32 {
-                position.x -= 1;
+            if (cube.x < 0) || (cube.x + cube.size as i32) > self.width as i32 {
+                vel.x *= -1;
             }
 
-            if (position.y + 50) > self.height as i32 {
-                position.y -= 1;
+            if (cube.y < 0) || (cube.y + cube.size as i32) > self.height as i32 {
+                vel.y *= -1;
+            }
+
+            for e in &self.entities {
+                if e == &ent {
+                    continue;
+                    // TODO: Not sure how to get a reference to the other Cubes
+                }
             }
         }
     }
@@ -68,16 +94,8 @@ impl<'a> System<'a> for Physics {
 
 pub fn main() -> Result<(), String> {
     let mut world = World::new();
-    world.register::<Position>();
-
-    world
-        .create_entity()
-        .with(Position { x: 100, y: 10 })
-        .build();
-    world
-        .create_entity()
-        .with(Position { x: 10, y: 50 })
-        .build();
+    world.register::<Cube>();
+    world.register::<Velocity>();
 
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
@@ -96,7 +114,29 @@ pub fn main() -> Result<(), String> {
     let mut physics = Physics {
         width: 800,
         height: 600,
+        entities: Vec::new(),
     };
+
+    let entity_count = 200;
+
+    for _ in 0..entity_count {
+        let size = rand::thread_rng().gen_range(0, 50);
+
+        let e = world
+            .create_entity()
+            .with(Cube {
+                x: rand::thread_rng().gen_range(0, 800 - size),
+                y: rand::thread_rng().gen_range(0, 600 - size),
+                size: size as u32,
+            })
+            .with(Velocity {
+                x: rand::thread_rng().gen_range(1, 5),
+                y: rand::thread_rng().gen_range(1, 5),
+            })
+            .build();
+
+        physics.entities.push(e)
+    }
 
     let mut event_pump = sdl_context.event_pump()?;
 
